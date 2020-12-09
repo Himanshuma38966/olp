0d73739704cfae38db5f06100e830f36e06e4c1b
Himanshuma38966 <74598469+Himanshuma38966@users.noreply.github.com>
2020-12-09 21:21:34 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,31 @@
+class Paper{
+    
+    constructor(x,y,radius,color) {
+        var options = { isStatic:false, 
+            restitution:0.3, 
+            friction:0.5,
+             density:1.2 }
+
+        this.body = Bodies.circle(x,y,radius,options);
+        this.radius= radius;
+        this.color = color;
+        this.image = loadImage("paper.png");
+        World.add(world,this.body);
+    }
+
+    display() {
+        var pos=this.body.position;
+        push();
+        translate(pos.x,pos.y);
+        rotate(this.body.angle);
+        ellipseMode(RADIUS);
+        
+        
+        ellipse(0,0,this.radius,this.radius);
+        
+        pop()
+        imageMode(CENTER);
+        image(this.image, pos.x,pos.y,  2*this.radius,2*this.radius);
+        
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,66 @@
+
+
+var box1,box2,box3;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render
+function preload()
+{
+	
+
+}
+
+function setup() {
+	createCanvas(1800, 1700);
+	
+	
+
+
+
+	//groundSprite=createSprite(width/2, height-35, width,10);
+	//groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	
+
+
+	//Create a Ground
+	//ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	//World.add(world, ground);
+
+	box2 = new Box(500,650,20,100,"red");
+	box3 = new Box(300,650,20,100,"red");
+	box1 = new Box(400,690,200,20,"red");
+	ground = new Ground(400,710,2500,20,"Brown")
+	ball= new Paper(100,690,20,"green");
+
+	
+	Engine.run(engine);
+}
+
+
+function draw() {
+  
+  background("white");
+  text(mouseX+","+mouseY,mouseX,mouseY);
+	
+
+	ball.display();
+	ground.display();
+	box1.display();
+	box2.display();
+	box3.display();
+	
+  
+}
+function keyPressed() {
+	console.log("click")
+	 if (keyCode === UP_ARROW) { 
+	Matter.Body.applyForce(ball.body,ball.body.position,{x:30,y:-85});
+ } }
